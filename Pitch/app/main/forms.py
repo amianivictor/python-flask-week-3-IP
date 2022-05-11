9c00f33d62ec866c77c782f6b4ba46dbb8e123bd
amianivictor <khahonjeri.amiani@student.moringaschool.com>
2022-05-11 09:54:41 +0300

    add main forms class vote

--- Pitch/app/main/forms.py
+++ Pitch/app/main/forms.py
@@ -16,3 +16,7 @@ class CommentForm(FlaskForm):
     comment = TextAreaField('Comment', validators=[Required()])
     submit = SubmitField('Post')
 
+
+class Vote(FlaskForm):
+    submit = SelectField('Like')
+
