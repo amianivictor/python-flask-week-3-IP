31be5759e0adc550d250ef1bdc9af5bd884add45
amianivictor <khahonjeri.amiani@student.moringaschool.com>
2022-05-11 09:53:26 +0300

    add main forms class postform

--- Pitch/app/main/forms.py
+++ Pitch/app/main/forms.py
@@ -3,3 +3,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    post = TextAreaField('Pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('PRODUCT', 'PRODUCT'), ('IDEA', 'IDEA'), ('Business', 'Business')],
+                           validators=[Required()])
+    submit = SubmitField('Post')
+
